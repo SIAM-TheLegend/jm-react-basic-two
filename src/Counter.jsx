ed52333200708ff1d5a9ada7c81871b877087910
Shahriar Hasan Siam <siamshahriarhasan@gmail.com>
2025-05-24 02:49:14 +0600

    styles added for Counter component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -13,7 +13,7 @@ function Counter() {
   }
 
   return (
-    <div>
+    <div style={{ border: "2px solid yellow", padding: "0 20px 20px", borderRadius: "6px" }}>
       <h3>Counter: {count}</h3>
       <button onClick={handleAdd}>Add</button>
       &nbsp;&nbsp;&nbsp;
